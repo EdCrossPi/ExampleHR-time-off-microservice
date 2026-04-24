80ea85ee28816a114c4b5ca028c8c5b9e675d943
EdCrossPi <eduardoxpimentel@gmail.com>
2026-04-23 21:05:39 -0300

    feat: configure TypeORM with better-sqlite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BalanceModule } from './balance/balance.module';
 import { TimeOffModule } from './time-off/time-off.module';
 import { HcmClientModule } from './hcm-client/hcm-client.module';
@@ -8,8 +7,18 @@ import { SyncModule } from './sync/sync.module';
 import { AuditModule } from './audit/audit.module';
 
 @Module({
-  imports: [BalanceModule, TimeOffModule, HcmClientModule, SyncModule, AuditModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'timeoff.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    BalanceModule,
+    TimeOffModule,
+    HcmClientModule,
+    SyncModule,
+    AuditModule,
+  ],
 })
 export class AppModule {}
